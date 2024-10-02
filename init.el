39a0f1000c3867f0fc9e00d846ef6d1f5d4e2686
Ryan Olson <ryolson@me.com>
2024-10-01 20:40:23 -0600

    Use tokyo night as theme

--- init.el
+++ init.el
@@ -10,23 +10,10 @@
   :bind (("C-x g" . magit-status)
          ("C-x C-g" . magit-status)))
 
-(set-cursor-color "#abcd74")
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#2d2925" :foreground "#c5b19b" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 180 :width normal :foundry "nil" :family "Menlo"))))
- '(font-lock-builtin-face ((t (:foreground "#e7c173"))))
- '(font-lock-comment-face ((t (:foreground "#abcd74"))))
- '(font-lock-constant-face ((t (:foreground "#e7c173"))))
- '(font-lock-doc-face ((t (:inherit font-lock-comment-face))))
- '(font-lock-function-name-face ((t nil)))
- '(font-lock-keyword-face ((t (:foreground "#fadfc2"))))
- '(font-lock-string-face ((t (:foreground "#df8d68"))))
- '(font-lock-type-face ((t (:foreground "#e7c173"))))
- '(font-lock-variable-name-face ((t nil)))
- '(org-block ((t (:extend t)))))
+(use-package doom-themes
+  :ensure t
+  :config
+  (load-theme 'doom-tokyo-night 'no-confirm))
 
 ;; Disabled modes
 (tool-bar-mode -1)
@@ -74,4 +61,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit)))
+ '(custom-safe-themes
+   '("4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d" "c1d5759fcb18b20fd95357dcd63ff90780283b14023422765d531330a3d3cec2" "d481904809c509641a1a1f1b1eb80b94c58c210145effc2631c1a7f2e4a2fdf4" "10e5d4cc0f67ed5cafac0f4252093d2119ee8b8cb449e7053273453c1a1eb7cc" "56044c5a9cc45b6ec45c0eb28df100d3f0a576f18eef33ff8ff5d32bac2d9700" "48042425e84cd92184837e01d0b4fe9f912d875c43021c3bcb7eeb51f1be5710" "34cf3305b35e3a8132a0b1bdf2c67623bc2cb05b125f8d7d26bd51fd16d547ec" "9d5124bef86c2348d7d4774ca384ae7b6027ff7f6eb3c401378e298ce605f83a" "2078837f21ac3b0cc84167306fa1058e3199bbd12b6d5b56e3777a4125ff6851" "7e377879cbd60c66b88e51fad480b3ab18d60847f31c435f15f5df18bdb18184" "4ade6b630ba8cbab10703b27fd05bb43aaf8a3e5ba8c2dc1ea4a2de5f8d45882" default))
+ '(package-selected-packages '(doom-themes magit)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
